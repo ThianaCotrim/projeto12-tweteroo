6c13f62c9382175abbec6618868fde1b3978c450
Thiana <thianacantunes@gmail.com>
2023-04-06 19:51:21 -0300

    versão com o post do sign-up rodando

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors"
+
 
 const tweets = [
 	{
@@ -8,10 +10,35 @@ const tweets = [
 	}
 ]
 
+
+
 const app = express();
+
+app.use(cors())
 
 app.get("/tweets", (req, res) => {
     res.send(tweets)
+})
+
+app.post("/sign-up", (req, res) => {
+
+	const usuario = []
+
+	const novoUsuario = {
+		    username: "bobesponja",
+			avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png"
+	}
+
+	// const usuarios = [];
+
+	// const { username, avatar } = req.body
+
+	// const novoUsuario = { username, avatar };
+
+    // usuarios.push(novoUsuario)
+	usuario.push(novoUsuario)
+    res.send("OK")
+	console.log(usuario)
 })
 
 app.listen(5000)
