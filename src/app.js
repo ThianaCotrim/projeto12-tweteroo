ea7cdd07eedb2e95c3fa1c7cac513236bfbc94c7
Thiana <thianacantunes@gmail.com>
2023-04-06 20:42:10 -0300

    versão com o post /tweets implementado

--- src/app.js
+++ src/app.js
@@ -10,14 +10,14 @@ const tweets = [
 	}
 ]
 
-
-
 const app = express();
 
 app.use(cors())
 
 app.get("/tweets", (req, res) => {
-    res.send(tweets)
+	
+		res.send(tweets)
+		
 })
 
 app.post("/sign-up", (req, res) => {
@@ -29,16 +29,20 @@ app.post("/sign-up", (req, res) => {
 			avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png"
 	}
 
-	// const usuarios = [];
-
-	// const { username, avatar } = req.body
-
-	// const novoUsuario = { username, avatar };
-
-    // usuarios.push(novoUsuario)
 	usuario.push(novoUsuario)
     res.send("OK")
 	console.log(usuario)
+})
+
+app.post("/tweets", (req, res) => {
+
+	const novoTweet = {
+		username: "bobesponja",
+	  	tweet: "Eu amo hambúrguer de siri!"
+	}
+
+	tweets.push(novoTweet)
+	res.send("OK")
 })
 
 app.listen(5000)
