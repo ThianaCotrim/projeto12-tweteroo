44d239644552a149d11c14190551d624c5c6bab4
Thiana <thianacantunes@gmail.com>
2023-04-07 20:30:24 -0300

    versão implemetado o /tweets post

--- src/app.js
+++ src/app.js
@@ -2,54 +2,47 @@ import express from "express";
 import cors from "cors"
 
 
-const tweets = [
-	{
-		username: "bobesponja",
-		avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
-		tweet: "Eu amo hambúrguer de siri!"
-	}
-]
+const tweets = []
+const usuario = []
 
-const tweeets = []
-const users = []
-
+// Criação do app
 const app = express();
 
+//Configuração do app
+app.use(express.json())
 app.use(cors())
 
-app.get("/tweets", (req, res) => {
-	
-	if (tweets === []) {
-		res.send(tweeets)
-	} else {
-		res.send(tweets)
-	}
 
-		
-		
+app.get("/tweets", (req, res) => {
+
+	
+	
+		res.send(tweets).status(200)
+	
 })
 
 app.post("/sign-up", (req, res) => {
 
-	const usuario = []
+	const {username, avatar} = req.body
 
-	const novoUsuario = {
-		    username: "bobesponja",
-			avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png"
-	}
+	const novoUsuario = {username, avatar}
 
 	usuario.push(novoUsuario)
+	console.log(req.body)	
     res.send("OK")
-	console.log(usuario)
-})
+	
+  });
 
 app.post("/tweets", (req, res) => {
 
 
-	const novoTweet = {
-		username: "bobesponja",
-	  	tweet: "Eu amo hambúrguer de siri!"
+	const {username, tweet} = req.body
+
+	if(!usuario.find((user) => user.username === username)){
+		return res.status(401).send("UNAUTHORIZED")
 	}
+
+	const novoTweet = {username, tweet}
 
 	tweets.push(novoTweet)
 	res.send("OK")
